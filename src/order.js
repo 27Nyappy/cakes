6794ccddd0c75aa0816fe5271e4e23ae3988ff72
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-07-23 17:52:42 -0700

    added export example shopping data

---
+++ src/order.js
@@ -0,0 +1,40 @@
+const order = [
+    {
+        code: 'almond',
+        quantity: 2,
+    },
+    {
+        code: 'carrot',
+        quantity: 1,
+    },
+    {
+        code: 'chocolate',
+        quantity: 3,
+    },
+    {
+        code: 'mango',
+        quantity: 4,
+    },
+    {
+        code: 'mousse',
+        quantity: 1,
+    },
+    {
+        code: 'passion-fruit',
+        quantity: 2,
+    },
+    {
+        code: 'red-velvet',
+        quantity: 2,
+    },
+    {
+        code: 'tiramisu',
+        quantity: 2,
+    },
+    {
+        code: 'vanilla',
+        quantity: 2,
+    }
+];
+
+export default order;
